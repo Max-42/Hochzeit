c9cc6d460eb5163294e79174c0821d3ffe143c70
max-42 <max@oppermann.fun>
2023-05-31 14:38:13 +0200

    fixes

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ text_size = 100
 subtext_size = 75
 date_subtext_size = 75
 
-correction_y = -40
+correction_y = -35
 line_spacing = 20
 
 #truefont pfade
@@ -37,8 +37,14 @@ output_path = 'output'
 #collage settings
 #Wie viele untersetzer passen in einer horizontalen reihe neben einander in den Laser?
 collage_nr_horizontal = 6 
-collage_horizontal_distance_px=200
-collage_vertical_distance_px=20
+collage_horizontal_padding=200
+collage_vertical_padding=200
+
+
+#the space added at the top left corner of the final collage
+# default is 0
+null_x_ofset = 200
+null_y_ofset = 200
 
 
 
@@ -60,6 +66,7 @@ Created on Thu May  25 11:45:00 2023
 import os
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont, ImageOps
+import math
 
 #create output folder if it does not exist
 cwd = os.getcwd()
@@ -75,7 +82,7 @@ print("#Stage 1:")
 with open(input_path, 'r', encoding="UTF-8") as f:
     i=1
     for line in f:
-        print("Processing: " + line.strip())
+        print("    Processing: " + line.strip())
         #set the text
         text = line.strip()
         #i
@@ -147,29 +154,37 @@ with open(input_path, 'r', encoding="UTF-8") as f:
 
 print("#Stage 2:")
 
+#delete previous collage if it exists
+path_to_collage = os.path.join(cwd,"output","000-collage.png")
+path_to_template = os.path.join(cwd,"output","000-template.png")
+if os.path.isfile(path_to_collage):
+    os.remove(path_to_collage)
+
 
 #number of png in the output folder
 png_nr = len([name for name in os.listdir(real_output_path) if os.path.isfile(os.path.join(real_output_path, name))])
 collage_nr_vertical = int(np.ceil(png_nr/collage_nr_horizontal))
-print(str(png_nr)+" png files found in output with "+str(collage_nr_horizontal)+" horizontal makes "+str(collage_nr_vertical)+" vertical collages")
+print("    "+str(png_nr)+" png files found in  the output foulder, that means with "+str(collage_nr_horizontal)+" horizontal colums/images. That results in "+str(collage_nr_vertical)+" vertical rows")
 
 collage_width =  collage_nr_horizontal*canvas_width
 collage_heigth = collage_nr_vertical*canvas_height
 
 #add padding
 
-collage_width_with_padding = collage_width + ((collage_nr_horizontal-1)*collage_horizontal_distance_px)
-collage_heigth_with_padding = collage_heigth + ((collage_nr_vertical-1)*collage_vertical_distance_px)
+collage_width_with_padding = collage_width + ((collage_nr_horizontal-1)*collage_horizontal_padding) + null_x_ofset
+collage_heigth_with_padding = collage_heigth + ((collage_nr_vertical)*collage_vertical_padding) + null_y_ofset
 
 collage = Image.new('RGBA', (collage_width_with_padding, collage_heigth_with_padding), (255, 255, 255))
 #for png file in output folder:
 
 #iterate through the output folder
-x=0
-y=0
+x=null_x_ofset
+y=null_y_ofset
 j=0
+couter = 0
 for filename in os.listdir(real_output_path):
     if filename.endswith(".png"):
+        couter+=1
         j+=1
         #set the path of the picture
         picture_path = os.path.join(real_output_path, filename)
@@ -180,17 +195,85 @@ for filename in os.listdir(real_output_path):
         #delete the picture
         #os.remove(picture_path)
         
-        print("space left: "+str(collage_width-x))
-        if x+canvas_width-collage_horizontal_distance_px > collage_width+1:
-            print("would hit the right border")
-            x=0
-            y+=canvas_height+collage_vertical_distance_px
-        else:
-            x+=canvas_width+collage_horizontal_distance_px
+        #set the x and y position for the next picture
+        x+=canvas_width+collage_horizontal_padding
+        if j == collage_nr_horizontal:
+            x=null_x_ofset
+            y+=canvas_height+collage_vertical_padding
+            j=0
+        continue
+    else:
+        continue
+
+
+
+    
+collage.save(path_to_collage)    
+
+print("#Stage 3:")
+
+#add a template for the outlies of the pictures
+
+# Create a new image with a white background
+image = Image.new("RGB", (canvas_width, canvas_height), "white")
+draw = ImageDraw.Draw(image)
+
+# Calculate the center coordinates of the hexagon
+center_x = image.width // 2
+center_y = image.height // 2
+
+# Calculate the length of each side of the hexagon
+side_length = 500
+
+# Calculate the coordinates of the hexagon vertices
+vertices = []
+for i in range(6):
+    angle_deg = 60 * i
+    angle_rad = math.radians(angle_deg)
+    x = center_x + side_length * math.cos(angle_rad)
+    y = center_y + side_length * math.sin(angle_rad)
+    vertices.append((x, y))
+
+# Draw the hexagon outline
+draw.polygon(vertices, outline="black", fill="blue")
+
+# Save the image
+path_to_hexagon=os.path.join(cwd,"output","000-hexagon.png")
+image.save(path_to_hexagon)
+
+print("#Stage 4:")
+#add a template for the outlies of the pictures
+template = Image.new('RGBA', (collage_width_with_padding, collage_heigth_with_padding), (255, 255, 255,255))
+
+#set the path of the picture
+
+#open the picture
+hexagon = Image.open(path_to_hexagon).convert('RGBA')
+x=null_x_ofset
+y=null_y_ofset
+j=0
+for l in range(couter):
+        j+=1
+        #set the path of the picture
         
-        #collage.save(os.path.join(cwd,"output","step-" +str(j) +".png"))
-path_to_collage = os.path.join(cwd,"output","000-collage.png")
-collage.save(path_to_collage)        
+        #open the picture
+        hexagon = Image.open(path_to_hexagon).convert('RGBA')
+        #paste the picture into the collage
+        template.paste(hexagon, (x, y), hexagon)
+        #delete the picture
+        #os.remove(picture_path)
+        
+        #set the x and y position for the next picture
+        x+=canvas_width+collage_horizontal_padding
+        if j == collage_nr_horizontal:
+            x=null_x_ofset
+            y+=canvas_height+collage_vertical_padding
+            j=0
+        continue
+
+
+template.save(path_to_template)
+
 
 
 #todo fix bug with alpha channel in windows photo viewer
